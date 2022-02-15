e2aee1f2967596a03637db345810baf7ad54597b
DANIELPACHECORINCON <danielpacherincon@gmail.com>
2022-02-15 10:36:24 -0500

    se creo la funcion de registerUser

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,1 +1,9 @@
-//Usuario
+import userController from "../controllers/user.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/registerUser", userController.registerUser);
+
+
+export default router;
