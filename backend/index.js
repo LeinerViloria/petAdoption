fbf60991932de2df222b0f4e94c960fd22f65120
LeinerViloria <vilorialeiner2002@gmail.com>
2022-02-15 09:28:49 -0500

    Se registra un rol

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,18 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import db from './db/db.js';
 
+import role from './routes/role.js';
+import user from './routes/user.js';
+import animal from './routes/animal.js';
+import adoption from './routes/adoption.js';
+
 dotenv.config();
 
 const APP = express();
 APP.use(express.json());
 APP.use(cors());
+
+APP.use("/api/role", role);
 
 APP.listen(process.env.PORT, ()=>console.log("Running in the port: ", process.env.PORT));
 
